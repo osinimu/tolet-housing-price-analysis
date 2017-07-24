526e6f31d6754431b6875d0850b1c2a04aecadfc
osisieke <christopherajulo@gmail.com>
2017-07-24 02:06:24 +0100

    Firt commit

---
+++ tolet/items.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+from scrapy.item import Item, Field
+
+
+class ToletItem(Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    property_name = Field()
+    Address = Field()
+    Price = Field()
+    
+   
